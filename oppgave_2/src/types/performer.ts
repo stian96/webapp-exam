43afff4c54d71bb791c090c79b6ee84ed52adcdf
stian96 <stiankru@hiof.no>
2023-11-09 14:08:29 +0100

    implemented function "calculateHeartRateZone".

--- oppgave_2/src/types/performer.ts
+++ oppgave_2/src/types/performer.ts
@@ -7,3 +7,13 @@ export type Performer = {
     watt: number,
     speed: number,
 }
+
+// Intensity zones in percentages. 
+const intensityZones = [50, 60, 70, 80, 90]
+
+// Returns an overview of the number of heartbeats per minute for the 5 intensity zones.
+export const calculateHeartRateZone = (maxHeartRate: number) => {
+    return intensityZones.map(zone => maxHeartRate * (zone / 100))
+}
+
+
