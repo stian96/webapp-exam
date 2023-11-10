11044343da691c5c2f94814457f784ba0a3c35ab
Sam Davies <samuelcd@hiof.no>
2023-11-10 12:01:16 +0100

    created API import users feature (without writing to database or deserialising)

--- oppgave_2/src/app/testpage/page.tsx
+++ oppgave_2/src/app/testpage/page.tsx
@@ -1,3 +1,17 @@
-export default function Home() {
-  return <div>Test Page!</div>
+import importAllUsers from "@/features/importUsers/importUsers.controller"
+
+const TestPage = async () => {
+  const response = await importAllUsers()
+  const result = response as { status: number; message: string }
+
+  return (
+    <main>
+      <div>
+        The API GET request finished with status code {result.status}. The
+        message is "{result.message}"
+      </div>
+    </main>
+  )
 }
+
+export default TestPage
---
+++ oppgave_2/src/features/importUsers/importUsers.controller.ts
@@ -0,0 +1,58 @@
+// A class that retrieves all users from a pre-defined URL in the JSON format. The
+
+type ApiResponse<T> = {
+    status: number;
+    message: T;
+}
+
+const importAll = async (): Promise<ApiResponse<string>> => {
+    try {
+
+        // List to contain several pages of API GET results.
+        let userData: unknown[] = [];
+        let pageCounter = 1;
+        // Paginated json document has a value called 'hasMore' if there are additional pages.
+        let hasMore = true;
+
+        // Loops as long as there are additional pages.
+        while (hasMore) {
+
+            const response = await fetch(`https://webapp-api.vercel.app/api/users?pageCounter=${pageCounter}`, {
+                method: "get",
+            });
+
+            if (response.ok) {
+
+                const responseData: unknown = await response.json();
+
+                // Adds the current pageCounter's data to the list.
+                userData = userData.concat(responseData.data);
+
+                // Iterates the pageCounter counter.
+                pageCounter++;
+
+                // Sets the boolean value of hasMore to reflect whether or not the URL has more data.
+                hasMore = responseData.hasMore;
+            } else {
+
+                return {
+                    status: response.status,
+                    message: "Error while retrieving from the URL using a GET request.",
+                };
+            }
+        }
+
+        return {
+            status: 200,
+            message: "Success",
+        };
+    } catch (error: unknown) {
+
+        return {
+            status: 500,
+            message: "Unexpected error while retrieving from the URL using a GET request.",
+        };
+    }
+};
+
+export default importAll;
