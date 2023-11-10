e85145d2b34b04e058b6c60f3cf020701749ceb2
stian96 <stiankru@hiof.no>
2023-11-10 12:47:57 +0100

    split the test up in separate units.

--- oppgave_2/src/tests/calculation.test.tsx
+++ oppgave_2/src/tests/calculation.test.tsx
@@ -2,19 +2,30 @@ import { calculateIntensityZones } from "@/types/performer";
 import { IntensityZone } from "@/types/performer"
 
 describe("Calculation Test", () => {
-    it("Should calculate intensity zone for heartrate", () => {
+    const maxHeartRate = 200
 
-        const maxHeartRate = 200
+    it("Should calculate intensity zone for heart rate in zone1", () => {
         const zone1 = calculateIntensityZones(maxHeartRate, IntensityZone.Zone1)
-        const zone2 = calculateIntensityZones(maxHeartRate, IntensityZone.Zone2)
-        const zone3 = calculateIntensityZones(maxHeartRate, IntensityZone.Zone3)
-        const zone4 = calculateIntensityZones(maxHeartRate, IntensityZone.Zone4)
-        const zone5 = calculateIntensityZones(maxHeartRate, IntensityZone.Zone5)
-
         expect(zone1 === (200 * (50 / 100)))
+    })
+
+    it("Should calculate intensity zone for heart rate in zone2", () => {
+        const zone2 = calculateIntensityZones(maxHeartRate, IntensityZone.Zone2)
         expect(zone2 === (200 * (60 / 100)))
+    }) 
+
+    it("Should calculate intensity zone for heart rate in zone3", () => {
+        const zone3 = calculateIntensityZones(maxHeartRate, IntensityZone.Zone3)
         expect(zone3 === (200 * (70 / 100)))
+    })
+
+    it("Should calculate intensity zone for heart rate in zone4", () => {
+        const zone4 = calculateIntensityZones(maxHeartRate, IntensityZone.Zone4)
         expect(zone4 === (200 * (80 / 100)))
+    })
+
+    it("Should calculate intensity zone for heart rate in zone5", () => {
+        const zone5 = calculateIntensityZones(maxHeartRate, IntensityZone.Zone5)
         expect(zone5 === (200 * (90 / 100)))
     })
 })
