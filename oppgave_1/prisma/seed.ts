062d78bcdbd608bb06a5a104abb487c28b3d6314
LorenaHolmeide <89775879+LorenaHolmeide@users.noreply.github.com>
2023-11-10 15:01:39 +0100

    updated to use db

--- oppgave_1/prisma/seed.ts
+++ oppgave_1/prisma/seed.ts
@@ -1,9 +1,143 @@
 import { PrismaClient } from "@prisma/client"
 
 const prisma = new PrismaClient()
+const taskData = [
+  {
+    text: "Skriv resultatet av regneoperasjonen", type: "add", data: "9|4",
+  },
+  {
+    text: "Skriv resultatet av regneoperasjonen", type: "divide", data: "10|2",
+  },
+  {
+    text: "Skriv resultatet av regneoperasjonen", type: "multiply", data: "8|7",
+  },
+  {
+    text: "Skriv resultatet av regneoperasjonen", type: "subtract", data: "9|5",
+  },
+  {
+    text: "Skriv resultatet av regneoperasjonen", type: "add", data: "5|9",
+  },
+  {
+    text: "Skriv resultatet av regneoperasjonen", type: "divide", data: "12|3",
+  },
+  {
+    text: "Skriv resultatet av regneoperasjonen", type: "multiply", data: "5|5",
+  },
+  {
+    text: "Skriv resultatet av regneoperasjonen", type: "subtract", data: "10|8",
+  },
+  {
+    text: "Skriv resultatet av regneoperasjonen", type: "add", data: "4|6",
+  },
+  {
+    text: "Skriv resultatet av regneoperasjonen", type: "divide", data: "9|3",
+  },
+  {
+    text: "Skriv resultatet av regneoperasjonen", type: "multiply", data: "4|2",
+  },
+  {
+    text: "Skriv resultatet av regneoperasjonen", type: "subtract", data: "50|8",
+  },
+  {
+    text: "Skriv resultatet av regneoperasjonen", type: "add", data: "4|6",
+  },
+  {
+    text: "Skriv resultatet av regneoperasjonen", type: "divide", data: "9|3",
+  },
+  {
+    text: "Skriv resultatet av regneoperasjonen", type: "multiply", data: "4|2",
+  },
+  {
+    text: "Skriv resultatet av regneoperasjonen", type: "subtract", data: "50|8",
+  },
+  {
+    text: "Skriv resultatet av regneoperasjonen", type: "add", data: "3|6",
+  },
+  {
+    text: "Skriv resultatet av regneoperasjonen", type: "divide", data: "4|3",
+  },
+  {
+    text: "Skriv resultatet av regneoperasjonen", type: "multiply", data: "5|2",
+  },
+  {
+    text: "Skriv resultatet av regneoperasjonen", type: "subtract", data: "56|8",
+  },
+  {
+    text: "Skriv resultatet av regneoperasjonen", type: "add", data: "43|6",
+  },
+  {
+    text: "Skriv resultatet av regneoperasjonen", type: "divide", data: "40|4",
+  },
+  {
+    text: "Skriv resultatet av regneoperasjonen", type: "multiply", data: "44|2",
+  },
+  {
+    text: "Skriv resultatet av regneoperasjonen", type: "subtract", data: "6|8",
+  },
+  {
+    text: "Skriv resultatet av regneoperasjonen", type: "add", data: "6|6",
+  },
+  {
+    text: "Skriv resultatet av regneoperasjonen", type: "divide", data: "70|3",
+  },
+  {
+    text: "Skriv resultatet av regneoperasjonen", type: "multiply", data: "7|2",
+  },
+  {
+    text: "Skriv resultatet av regneoperasjonen", type: "subtract", data: "8|8",
+  },
+  {
+    text: "Skriv resultatet av regneoperasjonen", type: "add", data: "9|6",
+  },
+  {
+    text: "Skriv resultatet av regneoperasjonen", type: "divide", data: "3|3",
+  },
+  {
+    text: "Skriv resultatet av regneoperasjonen", type: "multiply", data: "23|2",
+  },
+  {
+    text: "Skriv resultatet av regneoperasjonen", type: "subtract", data: "45|8",
+  },
+  {
+    text: "Skriv resultatet av regneoperasjonen", type: "add", data: "45|6",
+  },
+  {
+    text: "Skriv resultatet av regneoperasjonen", type: "divide", data: "55|3",
+  },
+  {
+    text: "Skriv resultatet av regneoperasjonen", type: "multiply", data: "46|2",
+  },
+  {
+    text: "Skriv resultatet av regneoperasjonen", type: "subtract", data: "65|8",
+  },
+  {
+    text: "Skriv resultatet av regneoperasjonen", type: "add", data: "67|6",
+  },
+  {
+    text: "Skriv resultatet av regneoperasjonen", type: "divide", data: "67|3",
+  },
+  {
+    text: "Skriv resultatet av regneoperasjonen", type: "multiply", data: "6|2",
+  },
+  {
+    text: "Skriv resultatet av regneoperasjonen", type: "subtract", data: "90|8",
+  },
+]
 
 async function main() {
-  console.log("Run")
+  for (const task of taskData) {
+    await prisma.task.create({
+      data: task,
+    });
+  }
 }
 
 main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
--- oppgave_1/src/app/api/restapi/route.ts
+++ oppgave_1/src/app/api/restapi/route.ts
@@ -1,132 +1,12 @@
 import type { NextRequest } from "next/server"
 import { NextResponse } from "next/server"
+import { PrismaClient } from '@prisma/client';
 
-import { type Task } from "@/types"
+const prisma = new PrismaClient();
 
-const tasks: Task[] = [
-  {
-    id: "124", text: "Skriv resultatet av regneoperasjonen", type: "add", data: "9|4",
-  },
-  {
-    id: "125", text: "Skriv resultatet av regneoperasjonen", type: "divide", data: "10|2",
-  },
-  {
-    id: "126", text: "Skriv resultatet av regneoperasjonen", type: "multiply", data: "8|7",
-  },
-  {
-    id: "127", text: "Skriv resultatet av regneoperasjonen", type: "subtract", data: "9|5",
-  },
-  {
-    id: "128", text: "Skriv resultatet av regneoperasjonen", type: "add", data: "5|9",
-  },
-  {
-    id: "129", text: "Skriv resultatet av regneoperasjonen", type: "divide", data: "12|3",
-  },
-  {
-    id: "130", text: "Skriv resultatet av regneoperasjonen", type: "multiply", data: "5|5",
-  },
-  {
-    id: "131", text: "Skriv resultatet av regneoperasjonen", type: "subtract", data: "10|8",
-  },
-  {
-    id: "132", text: "Skriv resultatet av regneoperasjonen", type: "add", data: "4|6",
-  },
-  {
-    id: "133", text: "Skriv resultatet av regneoperasjonen", type: "divide", data: "9|3",
-  },
-  {
-    id: "134", text: "Skriv resultatet av regneoperasjonen", type: "multiply", data: "4|2",
-  },
-  {
-    id: "135", text: "Skriv resultatet av regneoperasjonen", type: "subtract", data: "50|8",
-  },
-  {
-    id: "136", text: "Skriv resultatet av regneoperasjonen", type: "add", data: "4|6",
-  },
-  {
-    id: "134", text: "Skriv resultatet av regneoperasjonen", type: "divide", data: "9|3",
-  },
-  {
-    id: "135", text: "Skriv resultatet av regneoperasjonen", type: "multiply", data: "4|2",
-  },
-  {
-    id: "136", text: "Skriv resultatet av regneoperasjonen", type: "subtract", data: "50|8",
-  },
-  {
-    id: "137", text: "Skriv resultatet av regneoperasjonen", type: "add", data: "3|6",
-  },
-  {
-    id: "138", text: "Skriv resultatet av regneoperasjonen", type: "divide", data: "4|3",
-  },
-  {
-    id: "139", text: "Skriv resultatet av regneoperasjonen", type: "multiply", data: "5|2",
-  },
-  {
-    id: "140", text: "Skriv resultatet av regneoperasjonen", type: "subtract", data: "56|8",
-  },
-  {
-    id: "141", text: "Skriv resultatet av regneoperasjonen", type: "add", data: "43|6",
-  },
-  {
-    id: "142", text: "Skriv resultatet av regneoperasjonen", type: "divide", data: "40|4",
-  },
-  {
-    id: "143", text: "Skriv resultatet av regneoperasjonen", type: "multiply", data: "44|2",
-  },
-  {
-    id: "144", text: "Skriv resultatet av regneoperasjonen", type: "subtract", data: "6|8",
-  },
-  {
-    id: "145", text: "Skriv resultatet av regneoperasjonen", type: "add", data: "6|6",
-  },
-  {
-    id: "146", text: "Skriv resultatet av regneoperasjonen", type: "divide", data: "70|3",
-  },
-  {
-    id: "147", text: "Skriv resultatet av regneoperasjonen", type: "multiply", data: "7|2",
-  },
-  {
-    id: "148", text: "Skriv resultatet av regneoperasjonen", type: "subtract", data: "8|8",
-  },
-  {
-    id: "149", text: "Skriv resultatet av regneoperasjonen", type: "add", data: "9|6",
-  },
-  {
-    id: "150", text: "Skriv resultatet av regneoperasjonen", type: "divide", data: "3|3",
-  },
-  {
-    id: "151", text: "Skriv resultatet av regneoperasjonen", type: "multiply", data: "23|2",
-  },
-  {
-    id: "152", text: "Skriv resultatet av regneoperasjonen", type: "subtract", data: "45|8",
-  },
-  {
-    id: "153", text: "Skriv resultatet av regneoperasjonen", type: "add", data: "45|6",
-  },
-  {
-    id: "154", text: "Skriv resultatet av regneoperasjonen", type: "divide", data: "55|3",
-  },
-  {
-    id: "155", text: "Skriv resultatet av regneoperasjonen", type: "multiply", data: "46|2",
-  },
-  {
-    id: "156", text: "Skriv resultatet av regneoperasjonen", type: "subtract", data: "65|8",
-  },
-  {
-    id: "158", text: "Skriv resultatet av regneoperasjonen", type: "add", data: "67|6",
-  },
-  {
-    id: "159", text: "Skriv resultatet av regneoperasjonen", type: "divide", data: "67|3",
-  },
-  {
-    id: "160", text: "Skriv resultatet av regneoperasjonen", type: "multiply", data: "6|2",
-  },
-  {
-    id: "161", text: "Skriv resultatet av regneoperasjonen", type: "subtract", data: "90|8",
-  },
-]
 
-// TODO: Denne skal brukes til å "samle" svarene (om du ikke bruker database)
+// TODO: Denne skal brukes til å "samle" svarene (om du ikke bruker database)-Vi bruker db!
+/*
 const answers = new Map<Task["id"], { attempts: number }>()
 
 export function PUT(request: NextRequest) {
@@ -135,9 +15,9 @@ export function PUT(request: NextRequest) {
     return NextResponse.json({ success: false, error: "Invalid count" })
   return NextResponse.json({ success: true, data: tasks }, { status: 207 })
 }
+*/
 
-
-export function GET(request: NextRequest) {
+export async function GET(request: NextRequest) {
   const taskType = request.nextUrl.searchParams.get("type");
   const count = parseInt(request.nextUrl.searchParams.get("count") ?? '0', 10)
   if (!taskType) {
@@ -147,7 +27,13 @@ export function GET(request: NextRequest) {
     return NextResponse.json({ success: false, error: "Count value is not specified" }, { status: 400 });
 
   }
-  const filteredTasks = tasks.filter(task => task.type === taskType).slice(0, count);
+  //const filteredTasks = tasks.filter(task => task.type === taskType).slice(0, count);
+  const tasks = await prisma.task.findMany({
+    where: {
+      type: taskType
+    },
+    take: count
+  });
 
-  return NextResponse.json({ success: true, data: filteredTasks });
+  return NextResponse.json({ success: true, data: tasks });
 }
