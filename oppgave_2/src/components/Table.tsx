6e2cf68a29c35277921bb0f6e86e9075c2c9c874
stian96 <stiankru@hiof.no>
2023-11-11 15:49:03 +0100

    added sessions and reports page.

--- oppgave_2/src/components/Table.tsx
+++ oppgave_2/src/components/Table.tsx
@@ -1,9 +1,10 @@
+import Link from "next/link"
 import "../style/table.scss"
 
 const Table = () => {
 
     const performers = [
-        { name: "Perfomer A", gender: "Male", sport: "Running" },
+        { name: "Performer A", gender: "Male", sport: "Running" },
         { name: "Performer B", gender: "Female", sport: "Crossfit" },
         { name: "Performer C", gender: "Male", sport: "Football" },
         { name: "Performer D", gender: "Female", sport: "Handball" }
@@ -16,6 +17,8 @@ const Table = () => {
                     <tr key={index}>
                         <td className="table__body-data flex justify-between p-4">
                             {performer.name}
+                            <Link className="table__body-link" href="/sessions">Sessions</Link>
+                            <Link className="table__body-link" href="/reports">Reports</Link>
                             <button className="table__body-button" type="button">Edit</button>
                         </td>
                     </tr>
