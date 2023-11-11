26633d1531bc93b79ddbbc6cb9dd98f83eff2697
Sam Davies <samuelcd@hiof.no>
2023-11-11 14:30:16 +0100

    wrote functionality to write imported user to db - not tested yet

---
+++ oppgave_2/src/app/api/users/importUser/route.ts
@@ -0,0 +1,116 @@
+import { prisma } from "@/lib/prisma"
+import { type PerformerDto } from "@/types/DTO/importUsers";
+import { NextResponse, type NextRequest } from "next/server";
+
+
+export const PUT = async (request: NextRequest) => {
+
+  try {
+    console.log("Deserialising user.")
+    const data = await request.json()
+    const performer: PerformerDto = data as PerformerDto;
+    console.log("Deserialised user.")
+    
+    const newPerformer = await prisma.performers.create({
+      data: {
+          id: performer.id,
+          userId: performer.userId,
+          gender: performer.gender,
+          sport: performer.sport,
+          heartRate: performer.meta.heartrate,
+          watt: performer.meta.watt,
+          speed: performer.meta.speed
+      }
+    });
+
+    for (const activity of performer.activities) {
+
+      const newSession = await prisma.sessions.create({
+        data: {
+          name: activity.name,
+          isTemplate: false
+        }
+      });
+
+      if (activity.tags != null) {
+        for (const tag of activity.tags) {
+          const newTag = await prisma.sessionTags.create({
+            data: {
+              sessionId: newSession.id,
+              tag: tag
+            }
+          });
+        }
+      }
+      
+      if (activity.goalId != null) {
+        const newGoal = await prisma.goals.create({
+          data: {
+            id: activity.goalId,
+            isCompetition: false
+          }
+        });
+
+        const performerGoal = await prisma.performerGoals.create({
+          data: {
+            performerId: performer.id,
+            goalId: newGoal.id,
+            year: 2023
+          }
+        });
+
+        const newActivity = await prisma.sessionActivity.create({
+          data: {
+            date: activity.date,
+            sessionId: newSession.id,
+            goalId: activity.goalId,
+            performerId: performer.id
+          }
+        });
+      } else {
+        const newActivity = await prisma.sessionActivity.create({
+          data: {
+            date: activity.date,
+            sessionId: newSession.id,
+            performerId: performer.id
+          }
+        });
+      }
+
+      if (activity.questions != null) {
+        for (const question of activity.questions) {
+          const newQuestion = await prisma.questions.create({
+            data: {
+                id: question.id,
+                question: question.question,
+                type: question.type
+            }
+          });
+        }
+      }
+
+      for (const interval of activity.intervals) {
+        const newIntervals = await prisma.intervals.create({
+          data: {
+              id: interval.id,
+              duration: interval.duration,
+              intensity: interval.intensity
+          }
+        });
+
+      const newIntervalSessions = await prisma.sessionIntervals.create({
+        data: {
+            sessionId: newSession.id,
+            intervalId: interval.id
+        }
+      });
+    }
+    
+    console.log("Written user to database.")
+    return NextResponse.json({ success: true, message: "Success writing user to database." })
+  }
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json({ success: false, message: "Failed writing user to database." })
+  }
+}
--- oppgave_2/src/features/importUsers/importUsers.service.ts
+++ oppgave_2/src/features/importUsers/importUsers.service.ts
@@ -1,6 +1,9 @@
-import { type PerformerDto } from "@/types/DTO/importUsers";
+import { SessionActivity } from './../../types/sessions/sessionActivity';
+import { ActivityDto, type PerformerDto } from "@/types/DTO/importUsers";
 import { isPerformerExist, writePerformers } from "./importUsers.repository"
 import { Performer } from "@/types/performer";
+import { SessionActivity } from "@/types/sessions/sessionActivity";
+import { Goal } from "@/types/goal";
 
 
 export const writePerformersFromImport = async (jsonString: string): Promise<Result<string>> => {
@@ -49,4 +52,59 @@ export const createPerformerFromDto = (performerDto: PerformerDto): Performer =>
     }
 
     return performer
+}
+
+export const createSessionActivityFromDto = (performerDto: PerformerDto): SessionActivity[] => {
+
+    const activities: SessionActivity[] = []
+
+    performerDto.activities.forEach((activity) => {
+        const sessionActivity: SessionActivity = {
+        }
+
+        activities.push(sessionActivity)
+    })
+    const sessionActivity: SessionActivity = {
+
+        id          String     @id @default(cuid())
+        date        DateTime
+        session     Sessions   @relation(fields: [sessionId], references: [id])
+        sessionId   String
+        goal        Goals      @relation(fields: [goalId], references: [id])
+        goalId      String
+        performer   Performers @relation(fields: [performerId], references: [id])
+        performerId String
+        report      Reports?
+        id: cuid(),
+        date: activityDto.goalId,
+        session: performerDto.gender,
+        sport: performerDto.sport,
+        heartRate: performerDto.meta.heartrate,
+        watt: performerDto.meta.watt,
+        speed: performerDto.meta.speed
+    }
+
+    return sessionActivity
+}
+
+// tags
+
+// questions
+
+// intervals
+
+export const createGoalFromDto = (performerDto: PerformerDto): Goal => {
+    const goal: Goal = {
+        id: performerDto.activities[0].goal,
+        name: performerDto.activities[0].name,
+        date: performerDto.activities[0].date,
+        goal: performerDto.activities[0].goal,
+        comments: performerDto.activities[0].
+    }
+
+    return goal
+}
+
+function cuid() {
+    throw new Error("Function not implemented.");
 }
