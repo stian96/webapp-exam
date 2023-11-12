398ed7fe744f30fb2e83c6daf797d0667c489b01
stian96 <stiankru@hiof.no>
2023-11-12 22:18:53 +0100

    added a logo to the website.

--- oppgave_2/src/components/Header.tsx
+++ oppgave_2/src/components/Header.tsx
@@ -1,11 +1,16 @@
 import Navigation from "./Navigation"
+import Image from "next/image"
 
 import "../style/header.scss"
 
 const Header = () => {
+
     return(
     <div className="container flex justify-between items-center gap-7">
-        <h1 className="container__title">Dashboard</h1>
+        <div className="container__left flex items-center">
+            <Image className="container__logo" src="/logo.png" alt="Logo" width={60} height={60}/>
+            <h1 className="container__title">Dashboard</h1>
+        </div>
         <Navigation />
     </div>
     )
