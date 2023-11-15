571b2d3fbde1c7f8a7d020402c1b88321327a0fc
stian96 <stiankru@hiof.no>
2023-11-15 14:46:58 +0100

    changed Session to be a class instead of type.

--- oppgave_2/src/types/sessions/session.ts
+++ oppgave_2/src/types/sessions/session.ts
@@ -1,11 +1,27 @@
 import { Question } from "../question"
 import { Interval } from "../performance/interval"
 
-export type Session = {
-    id: string,
-    name: string,
-    type: string,
-    tags: Array<string>,
-    question: Array<Question>,
-    intervals: Array<Interval>
+export class Session {
+    id: string
+    name: string
+    type: string
+    tags: string[]
+    question: Question[]
+    intervals: Interval[]
+
+    constructor(
+        id: string,
+        name: string,
+        type: string,
+        tags: string[],
+        question: Question[],
+        intervals: Interval[]) {
+
+        this.id = id,
+        this.name = name,
+        this.type = type,
+        this.tags = tags,
+        this.question = question,
+        this.intervals = intervals
+    }
 }
