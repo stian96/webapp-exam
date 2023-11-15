9967b7ef91be3e80b78d06a31436a425bc8b17d1
stian96 <stiankru@hiof.no>
2023-11-15 22:02:03 +0100

    implemented logic for button click.

--- oppgave_2/src/components/tables/GoalsRow.tsx
+++ oppgave_2/src/components/tables/GoalsRow.tsx
@@ -1,7 +1,8 @@
+"use client"
 import GoalsData from "./GoalsData"
+import { useState } from "react"
 
 import "../../style/goalsRow.scss"
-import { d } from "msw/lib/glossary-de6278a9"
 
 type GoalsRowProps = {
     firstField: string,
@@ -9,27 +10,37 @@ type GoalsRowProps = {
 }
 
 const GoalsRow = ({ firstField, buttonText }: GoalsRowProps) => {
+    const [showGoalsData, setShowGoalsData] = useState(false)
+
     const rowData = [
         { id: "A", num: 1 },
         { id: "B", num: 2 },
         { id: "C", num: 3}
     ]
 
+    const toggleGoalsData = () => {
+        setShowGoalsData(!showGoalsData)
+    }
+
     return (
         <div className="test">
             <tr className="goals__body-row flex justify-between p-4">
                 <td className="goals__body-row-data">{ firstField }</td> 
                 <td>
-                    <button className="goals__body-row-button" type="button">{ buttonText }</button>
+                    <button className="goals__body-row-button" type="button" onClick={toggleGoalsData}>
+                        { buttonText }
+                    </button>
                 </td>
             </tr>
-            <tr className="goals__data-row flex justify-center p-4">
-                <td className="w-full mx-4" colSpan={3}>
-                    { rowData.map((data) => (
-                        <GoalsData id={data.id} goalNumber={data.num}/>
-                    ))}
-                </td>
-            </tr>
+            {showGoalsData && (
+                 <tr className="goals__data-row flex justify-center p-4">
+                 <td className="w-full mx-4" colSpan={3}>
+                     { rowData.map((data, index) => (
+                         <GoalsData key={index} id={data.id} goalNumber={data.num}/>
+                     ))}
+                 </td>
+             </tr>
+            )}
         </div>
     )
 }
