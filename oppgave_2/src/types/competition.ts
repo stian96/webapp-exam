bbc5a9894c4031561e9877774b4ac3c96395e341
stian96 <stiankru@hiof.no>
2023-11-15 14:39:56 +0100

    changed some types to class and interfaces.

--- oppgave_2/src/types/competition.ts
+++
@@ -1,8 +0,0 @@
-type Competition = {
-    name: string,
-    date: string,
-    place: string,
-    competitionType: string,
-    priority: PriorityEnum,
-    comment: string
-}
--- oppgave_2/src/types/interface/GoalModel.ts
+++ oppgave_2/src/types/interface/GoalModel.ts
@@ -1,4 +1,4 @@
-export type Goal = {
+export interface GoalModel {
     id: string,
     name: string,
     date: Date,
