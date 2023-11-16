5a9cfbfc124882e24b35dc0367379906eddd57d9
stian96 <stiankru@hiof.no>
2023-11-16 13:14:20 +0100

    created Activity component.

---
+++ oppgave_2/src/components/tables/Activity.tsx
@@ -0,0 +1,16 @@
+
+const Activity = () => {
+
+    return (
+        <div className="flex justify-between p-4">
+            <span>Checkbox</span>
+            <span>Activity</span>
+            <button>Duplicate</button>
+            <button>Edit</button>
+            <button>Delete</button>
+            <button>Create Report</button>
+        </div>
+    )
+}
+
+export default Activity
--- oppgave_2/src/components/tables/Compare.tsx
+++ oppgave_2/src/components/tables/Compare.tsx
@@ -12,6 +12,16 @@ const Compare = () => {
                         <Filters />
                     </td>
                 </tr>
+                <tr>
+                    <div className="flex justify-between p-4">
+                        <span>Checkbox</span>
+                        <span>Activity</span>
+                        <button>Duplicate</button>
+                        <button>Edit</button>
+                        <button>Delete</button>
+                        <button>Create Report</button>
+                    </div>
+                </tr>
             </tbody>
         </table>
     )
