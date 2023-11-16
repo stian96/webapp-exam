e0f6bf83d08ee9c5c5a991536bce851d2292f554
stian96 <stiankru@hiof.no>
2023-11-16 14:16:21 +0100

    implemented activity table.

--- oppgave_2/src/components/tables/Activity.tsx
+++ oppgave_2/src/components/tables/Activity.tsx
@@ -1,14 +1,27 @@
+import "../../style/activity.scss"
 
-const Activity = () => {
+type ActivityProps = {
+    id: string
+}
+
+const Activity = ({ id }: ActivityProps) => {
+    const buttons = ["Duplicate", "Edit", "Delete", "Create Report"]
 
     return (
-        <div className="flex justify-between p-4">
-            <span>Checkbox</span>
-            <span>Activity</span>
-            <button>Duplicate</button>
-            <button>Edit</button>
-            <button>Delete</button>
-            <button>Create Report</button>
+        <div className="activity-cont flex justify-between items-center gap-8 px-8">
+            <input className="activity-cont__checkbox p-2" 
+                type="checkbox" 
+                id="activityCheckbox" 
+                name="activityCheckbox" 
+            />
+            <div className="activity flex justify-between w-full">
+                <span className="activity__id">{ id }</span>
+                <div className="activity__container flex gap-5">
+                    {buttons.map((name, index) => (
+                        <button className="activity__container-button" key={index}>{name}</button>
+                    ))}
+                </div>
+            </div>
         </div>
     )
 }
--- oppgave_2/src/components/tables/Compare.tsx
+++ oppgave_2/src/components/tables/Compare.tsx
@@ -1,9 +1,12 @@
 import Filters from "./Filters"
+import Activity from "./Activity"
 import "../../style/compare.scss"
 
 const Compare = () => {
+    const activities = ["Activity 1", "Activity 2", "Activity 3"]
+    
     return (
-        <table className="compare mt-5">
+        <table className="compare">
             <tbody className="compare__body">
                 <tr className="compare__body-row flex justify-between items-center">
                     <td className="compare__body-data">Compare</td>
@@ -12,15 +15,12 @@ const Compare = () => {
                         <Filters />
                     </td>
                 </tr>
-                <tr>
-                    <div className="flex justify-between p-4">
-                        <span>Checkbox</span>
-                        <span>Activity</span>
-                        <button>Duplicate</button>
-                        <button>Edit</button>
-                        <button>Delete</button>
-                        <button>Create Report</button>
-                    </div>
+                <tr className="activity-table ">
+                    <td>
+                        { activities.map((activity, index) =>  (
+                            <Activity key={index} id={activity} />
+                        ))}
+                    </td>
                 </tr>
             </tbody>
         </table>
