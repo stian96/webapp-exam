c021f44c9b4bed323a103e50c30040e352da49ed
stian96 <stiankru@hiof.no>
2023-11-17 14:34:30 +0100

    added overlay to popup

--- oppgave_2/src/components/EditPopup.tsx
+++ oppgave_2/src/components/EditPopup.tsx
@@ -23,51 +23,53 @@ const EditPopup = ({ editPerformer, setEditPerformer, handleSave }: EditPopupPro
     return(
     <>
         <button className="button float-right" onClick={() => setIsPopupOpen(true)}>Edit</button>
-        <Popup open={isPopupOpen} closeOnDocumentClick onClick={closePopup}>
-                <div className="modal">
-                    <button className="modal__close float-right" onClick={closePopup}>
-                        &times;
-                    </button>
-                    <h1 className="modal__header">Edit Performer</h1>
-                    <div className="modal__content flex flex-col mt-16">
-                        <label className="modal__content-label">Name:</label>
-                        <input 
-                            className="modal__content-input" 
-                            type="text" name="name" 
-                            placeholder="..." 
-                            value={editPerformer.name}
-                            onChange={handleChange} 
-                        />
-                        <label className="modal__content-label">Gender:</label>
-                        <input 
-                            className="modal__content-input" 
-                            type="text" 
-                            name="gender" 
-                            placeholder="..." 
-                            value={editPerformer.gender}
-                            onChange={handleChange} 
-                        />
-                        <label className="modal__content-label">Sport:</label>
-                        <input 
-                            className="modal__content-input" 
-                            type="text" 
-                            name="sport" 
-                            placeholder="..." 
-                            value={editPerformer.sport}
-                            onChange={handleChange} 
-                        /> 
-                    </div>
-                    <div className="modal__actions">
-                        <button 
-                            className="modal__actions-button" 
-                            onClick={() => {
-                                handleSave()
-                                closePopup()
-                            }}>Save
+        <div className={`overlay ${isPopupOpen ? 'overlay--active' : ''}`}>
+            <Popup open={isPopupOpen} closeOnDocumentClick onClick={closePopup}>
+                    <div className="modal">
+                        <button className="modal__close float-right" onClick={closePopup}>
+                            &times;
                         </button>
+                        <h1 className="modal__header">Edit Performer</h1>
+                        <div className="modal__content flex flex-col mt-16">
+                            <label className="modal__content-label">Name:</label>
+                            <input 
+                                className="modal__content-input" 
+                                type="text" name="name" 
+                                placeholder="..." 
+                                value={editPerformer.name}
+                                onChange={handleChange} 
+                            />
+                            <label className="modal__content-label">Gender:</label>
+                            <input 
+                                className="modal__content-input" 
+                                type="text" 
+                                name="gender" 
+                                placeholder="..." 
+                                value={editPerformer.gender}
+                                onChange={handleChange} 
+                            />
+                            <label className="modal__content-label">Sport:</label>
+                            <input 
+                                className="modal__content-input" 
+                                type="text" 
+                                name="sport" 
+                                placeholder="..." 
+                                value={editPerformer.sport}
+                                onChange={handleChange} 
+                            /> 
+                        </div>
+                        <div className="modal__actions">
+                            <button 
+                                className="modal__actions-button" 
+                                onClick={() => {
+                                    handleSave()
+                                    closePopup()
+                                }}>Save
+                            </button>
+                        </div>
                     </div>
-                </div>
-        </Popup>
+            </Popup>
+        </div>
     </>
     )
 }
