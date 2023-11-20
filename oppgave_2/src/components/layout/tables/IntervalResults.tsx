e04aec0d8e80653d35fa2afd8cbab0198b8319f8
Sammish <samd93@hotmail.co.uk>
2023-11-20 17:06:34 +0100

    corrected bug involving overwriting scss table file with analysistable file

--- oppgave_2/src/components/layout/tables/IntervalResults.tsx
+++ oppgave_2/src/components/layout/tables/IntervalResults.tsx
@@ -17,13 +17,13 @@ const IntervalResults = ({
 }: IntervalResultsProps) => {
   return (
     <div className="overflow-x-auto">
-      <table className="table min-w-full">
-        <thead className="table__head">
+      <table className="analysisTable min-w-full">
+        <thead className="analysisTable__head">
           <tr>
-            <th className="table__head-data">Activity ID</th>
+            <th className="analysisTable__head-data">Activity ID</th>
             {colNames.map((name, index) => (
               <th
-                className={`table__head-data ${
+                className={`analysisTable__head-data ${
                   filteredColumns.includes(index) && "hidden"
                 }`}
                 key={name}
@@ -31,7 +31,7 @@ const IntervalResults = ({
                 <span className="flex items-center space-x-2">
                   <span>{name}</span>
                   <button
-                    className={`table__body-button text-white ${
+                    className={`analysisTable__body-button text-white ${
                       filteredColumns.includes(index) && "hidden"
                     }`}
                     onClick={() => {
@@ -45,91 +45,91 @@ const IntervalResults = ({
             ))}
           </tr>
         </thead>
-        <tbody className="table__body">
+        <tbody className="analysisTable__body">
           {intervalList.map((interval, index) => (
             <tr key={index}>
-              <td className="table__body-id line-clamp-2">
+              <td className="analysisTable__body-id line-clamp-2">
                 {interval.activityId}
               </td>
               <td
-                className={`table__body-data ${
+                className={`analysisTable__body-data ${
                   filteredColumns.includes(0) && "hidden"
                 }`}
               >
                 {interval.intensity.min}
               </td>
               <td
-                className={`table__body-data ${
+                className={`analysisTable__body-data ${
                   filteredColumns.includes(1) && "hidden"
                 }`}
               >
                 {interval.intensity.max}
               </td>
               <td
-                className={`table__body-data ${
+                className={`analysisTable__body-data ${
                   filteredColumns.includes(2) && "hidden"
                 }`}
               >
                 {interval.intensity.average}
               </td>
               <td
-                className={`table__body-data ${
+                className={`analysisTable__body-data ${
                   filteredColumns.includes(3) && "hidden"
                 }`}
               >
                 {interval.pulse.min}
               </td>
               <td
-                className={`table__body-data ${
+                className={`analysisTable__body-data ${
                   filteredColumns.includes(4) && "hidden"
                 }`}
               >
                 {interval.pulse.max}
               </td>
               <td
-                className={`table__body-data ${
+                className={`analysisTable__body-data ${
                   filteredColumns.includes(5) && "hidden"
                 }`}
               >
                 {interval.pulse.average}
               </td>
               <td
-                className={`table__body-data ${
+                className={`analysisTable__body-data ${
                   filteredColumns.includes(6) && "hidden"
                 }`}
               >
                 {interval.speed.min}
               </td>
               <td
-                className={`table__body-data ${
+                className={`analysisTable__body-data ${
                   filteredColumns.includes(7) && "hidden"
                 }`}
               >
                 {interval.speed.max}
               </td>
               <td
-                className={`table__body-data ${
+                className={`analysisTable__body-data ${
                   filteredColumns.includes(8) && "hidden"
                 }`}
               >
                 {interval.speed.average}
               </td>
               <td
-                className={`table__body-data ${
+                className={`analysisTable__body-data ${
                   filteredColumns.includes(9) && "hidden"
                 }`}
               >
                 {interval.watt.min}
               </td>
               <td
-                className={`table__body-data ${
+                className={`analysisTable__body-data ${
                   filteredColumns.includes(10) && "hidden"
                 }`}
               >
                 {interval.watt.max}
               </td>
               <td
-                className={`table__body-data ${
+                className={`analysisTable__body-data ${
                   filteredColumns.includes(11) && "hidden"
                 }`}
               >
