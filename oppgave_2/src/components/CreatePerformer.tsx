7da14c3b8974b2409b65cfc6efd0406cab9f0a11
Ashti Mawloudi <111912492+a5h71@users.noreply.github.com>
2023-11-27 14:39:24 +0100

    added input validation

--- oppgave_2/src/components/CreatePerformer.tsx
+++ oppgave_2/src/components/CreatePerformer.tsx
@@ -15,6 +15,17 @@ const CreatePerformer = () => {
 
     const [submitButtonText, setSubmitButtonText] = useState('Create Performer');
 
+    const [errors, setErrors] = useState({
+      userId: '',
+      gender: '',
+      sportType: '',
+      heartRate: '',
+      watt: '',
+      speed: ''
+    });
+    
+
+
 
     
     const handleIdChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -50,53 +61,101 @@ const CreatePerformer = () => {
     setSpeed('');
   };
 
-
-    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-      e.preventDefault();
-
-      setSubmitButtonText('Saving...');
-
-      const heartRateInt = parseInt(heartRate, 10);
-      const wattInt = parseInt(watt, 10);
-      const speedInt = parseInt(speed, 10);
+  const validateFields = () => {
+    const newErrors = {};
+    let isValid = true;
   
-      
-      if (isNaN(heartRateInt) || isNaN(wattInt) || isNaN(speedInt)) {
-          console.error("Invalid input: heartRate, watt, or speed is not a number.");
-          return; 
-      }
-      const newPerformer = { 
-        userId: performerId, 
-        gender, 
-        sport: sportType, 
-        heartRate: heartRateInt, 
-        watt: wattInt, 
-        speed: speedInt 
+    const fieldValues = {
+      userId: performerId,
+      gender: gender,
+      sportType: sportType,
+      heartRate: heartRate,
+      watt: watt,
+      speed: speed
     };
-
-    try {
-      const response = await fetch('/api/users/createUser', {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(newPerformer),
-      });
-
-      if (response.ok) {
-          setSubmitButtonText('Performer Saved!');
-          clearForm();
-          
-      } else {
-          setSubmitButtonText('Error: Could not save');
+  
+    const fieldsToValidate = {
+      userId: 'User ID',
+      gender: 'Gender',
+      sportType: 'Sport Type',
+      heartRate: 'Heart Rate',
+      watt: 'Watt',
+      speed: 'Speed'
+    };
+  
+    for (const [key, displayName] of Object.entries(fieldsToValidate)) {
+      if (!fieldValues[key].trim()) {
+        newErrors[key] = `${displayName} is required`;
+        isValid = false;
       }
-  } catch (error) {
-      setSubmitButtonText(`Error: ${error.message}`);
-  }
+    }
+  
+    setErrors(newErrors);
+    return isValid;
+  };
 
+  const prepareFormData = () => {
+    const heartRateInt = parseInt(heartRate, 10);
+    const wattInt = parseInt(watt, 10);
+    const speedInt = parseInt(speed, 10);
+  
+    if (isNaN(heartRateInt) || isNaN(wattInt) || isNaN(speedInt)) {
+      console.error("Invalid input: heartRate, watt, or speed is not a number.");
+      return null;
+    }
+  
+    return {
+      userId: performerId, 
+      gender, 
+      sport: sportType, 
+      heartRate: heartRateInt, 
+      watt: wattInt, 
+      speed: speedInt 
+    };
   };
   
+  const submitPerformerData = async (formData) => {
+    try {
+      const response = await fetch('/api/users/createUser', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
   
+      if (response.ok) {
+        setSubmitButtonText('Performer Saved!');
+        clearForm();
+      } else {
+        const resData = await response.json();
+        setSubmitButtonText('Error: ' + resData.message);
+        
+        if (resData.errors) {
+          setErrors(resData.errors);
+        }
+      }
+    } catch (error) {
+      setSubmitButtonText(`Error: ${error.message}`);
+    }
+  };
+  
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setSubmitButtonText('Saving...');
+  
+    if (!validateFields()) {
+      setSubmitButtonText('Create Performer');
+      return;
+    }
+  
+    const formData = prepareFormData();
+    if (!formData) {
+      setSubmitButtonText('Create Performer');
+      return;
+    }
+  
+    await submitPerformerData(formData);
+  };
 
     return (
       <div className='create-performer'>
@@ -104,24 +163,27 @@ const CreatePerformer = () => {
             <label>
                 User ID:
                 <input type="text" id= "performerId" value={performerId} onChange={handleIdChange} className='form__input --default'/>
+                {errors.userId && <span className="create-performer-error-message">{errors.userId}</span>}
             </label>
             <br />
             <div className="create-performer-form-row">
                 <div className="create-performer-form-field">
-                    <label>
-                        Gender:
-                        <select value={gender} id= "genederSelection" onChange={handleGenderChange} className='create-performer-form__select'>
+                    <label htmlFor='genderSelection'>
+                        Gender: </label>
+                        <select value={gender} id= "genderSelection" onChange={handleGenderChange} className='create-performer-form__select'>
                             <option value="">Select Gender</option>
                             <option value="male">Male</option>
                             <option value="female">Female</option>
                             <option value="other">Other</option>
                         </select>
-                    </label>
+                   
                 </div>
+                {errors.gender && <span className="create-performer-error-message">{errors.gender}</span>}
                 <div className="create-performer-form-field">
-                    <label>
-                        Sport Type:
+                    <label htmlFor='sportTypeSelection'>
+                        Sport Type:  </label>
                         <select value={sportType} id= "sportTypeSelection" onChange={handleSportTypeChange} className='create-performer-form__select'>
+                        
                             <option value="">Select Sport Type</option>
                             <option value="running">Running</option>
                             <option value="cycling">Cycling</option>
@@ -132,23 +194,27 @@ const CreatePerformer = () => {
                             <option value="other">Other</option>
                                     
                         </select>
-                    </label>
+                    
                 </div>
+                {errors.sportType && <span className="create-performer-error-message">{errors.sportType}</span>}
             </div>
             <br />
             <label>
               Heart Rate:
               <input type="text" id="heartRate" value={heartRate} onChange={handleHeartRateChange} className='form__input --default'/>
+              {errors.heartRate && <span className="create-performer-error-message">{errors.heartRate}</span>}
             </label>
             <br />
             <label>
               Watt:
               <input type="text" id="watt" value={watt} onChange={handleWattChange} className='form__input --default'/>
+              {errors.watt && <span className="create-performer-error-message">{errors.watt}</span>}
             </label>
             <br />
             <label>
               Speed:
               <input type="text" id= "speed" value={speed} onChange={handleSpeedChange} className='form__input --default'/>
+              {errors.speed && <span className="create-performer-error-message">{errors.speed}</span>}
             </label>
             <br />
             <button
