8675ae54a1e79f59297ed92ec40605ebbb94d83a
stian96 <stiankru@hiof.no>
2023-11-28 19:30:39 +0100

    implemented layout and logic for IntensityResult

--- oppgave_2/src/components/data/IntensityResult.tsx
+++ oppgave_2/src/components/data/IntensityResult.tsx
@@ -11,9 +11,42 @@ type IntensityResultProps = {
 
 const IntensityResult = ({ results, selectedOptions }: IntensityResultProps) => {
 
+    const heartRateList = results.heartRate as number[]
+    const speedList = results.speed as number[]
+    const wattList = results.watt as number[]
+
     return (
         <div className="intensity-results">
-
+            { selectedOptions.heartRate && heartRateList.length > 0 && (
+                <>
+                    <h1>Result of Pulse Zones</h1>
+                    { heartRateList.map((item, index) => (
+                        <span key={`hr-zone-${index}`} >
+                            Zone {index + 1}: {item.toFixed(2)}
+                        </span>
+                    ))}
+                </>
+            )}
+            {selectedOptions.speed && speedList.length > 0 && (
+                <>
+                    <h1>Result of Pulse Zones</h1>
+                    { speedList.map((item, index) => (
+                        <span key={`speed-zone-${index}`} >
+                            Zone {index + 1}: {item.toFixed(2)}
+                        </span>
+                    ))}
+                </>
+            )}
+            {selectedOptions.watt && wattList.length > 0 && (
+                <>
+                    <h1>Result of Pulse Zones</h1>
+                    { wattList.map((item, index) => (
+                        <span key={`watt-zone-${index}`} >
+                            Zone {index + 1}: {item.toFixed(2)}
+                        </span>
+                    ))}
+                </>
+            )}
         </div>
     )
 }
