e19dae8e5a9e365caac6feb3f9c1c2eb6ec1f33e
stian96 <stiankru@hiof.no>
2023-11-28 16:00:21 +0100

    imlemented dropdown meny for intensity zones

--- oppgave_2/src/components/layout/popups/IntensityPopup.tsx
+++ oppgave_2/src/components/layout/popups/IntensityPopup.tsx
@@ -3,6 +3,7 @@ import { useState } from "react"
 import CheckBox from "../CheckBox"
 import Popup from "reactjs-popup"
 import "@/style/popup.scss"
+import { IntensityZone } from "@/types/performer"
 
 type IntensityProps = {
     header: string
@@ -11,6 +12,8 @@ type IntensityProps = {
 }
 
 const IntensityPopup = ({ header, isOpen, onClose }: IntensityProps) => {
+    const [selectedZone, setSelectedZone] = useState<IntensityZone | "all">(IntensityZone.Zone1)
+    const [nextClicked, setNextClicked] = useState(false)
     const [selectedOptions, setSelectedOptions] = useState({
         heartRate: false,
         speed: false,
@@ -22,6 +25,8 @@ const IntensityPopup = ({ header, isOpen, onClose }: IntensityProps) => {
         setSelectedOptions({ ...selectedOptions, [name.id]: name.checked })
     }
 
+    const handleClick = () => setNextClicked(!nextClicked)
+
     return (
         <div className={`overlay ${isOpen ? 'overlay-active' : ''}`}>
             <Popup open={isOpen} closeOnDocumentClick={false}>
@@ -30,13 +35,27 @@ const IntensityPopup = ({ header, isOpen, onClose }: IntensityProps) => {
                         &times;
                     </button>
                     <h1 className="modal__header">{header}</h1>
-                    <div className="modal__content py-8 flex justify-center">
-                        <CheckBox id="heartRate" value="Pulse" onChange={handleChange} checked={selectedOptions.heartRate} />
-                        <CheckBox id="speed" value="Speed" onChange={handleChange} checked={selectedOptions.speed} />
-                        <CheckBox id="watt" value="Watt" onChange={handleChange} checked={selectedOptions.watt} />
-                    </div>
+                    { !nextClicked && (
+                        <div className="modal__content py-8 flex justify-center">
+                            <CheckBox id="heartRate" value="Pulse" onChange={handleChange} checked={selectedOptions.heartRate} />
+                            <CheckBox id="speed" value="Speed" onChange={handleChange} checked={selectedOptions.speed} />
+                            <CheckBox id="watt" value="Watt" onChange={handleChange} checked={selectedOptions.watt} />
+                        </div>
+                    )} 
+                    { nextClicked && (
+                        <div className="modal__content py-8 flex justify-center">
+                            <select value={selectedZone} onChange={e => setSelectedZone(e.target.value as IntensityZone)}>
+                                <option value="all">All Zones</option>
+                                <option value={IntensityZone.Zone1}>Zone 1</option>
+                                <option value={IntensityZone.Zone2}>Zone 2</option>
+                                <option value={IntensityZone.Zone3}>Zone 3</option>
+                                <option value={IntensityZone.Zone4}>Zone 4</option>
+                                <option value={IntensityZone.Zone5}>Zone 5</option>
+                            </select>
+                        </div>
+                    )}
                     <div className="modal__actions">
-                        <button className="modal__actions-button">
+                        <button className="modal__actions-button" onClick={handleClick}>
                             Next
                         </button>
                     </div>
@@ -44,7 +63,6 @@ const IntensityPopup = ({ header, isOpen, onClose }: IntensityProps) => {
             </Popup>
         </div>
     )
-
 }
 
 export default IntensityPopup
