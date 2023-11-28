61320150bb7499a6dc438b9bc201f170a578f563
stian96 <stiankru@hiof.no>
2023-11-28 20:07:49 +0100

    fixed design a bit.

--- oppgave_2/src/components/data/IntensityResult.tsx
+++ oppgave_2/src/components/data/IntensityResult.tsx
@@ -1,6 +1,8 @@
 import { IntensityZone } from "@/types/performer";
 import { CalculationResults } from "../layout/popups/IntensityPopup";
 
+import "@/style/intensityResult.scss"
+
 type IntensityResultProps = {
     results: CalculationResults
     selectedOptions: {
@@ -19,37 +21,55 @@ const IntensityResult = ({ results, selectedOptions, selectedZone }: IntensityRe
 
     const isAllZone = selectedZone === "all"
 
+    const mapSelectedZone = (zone: IntensityZone | "all") => {
+        let result = ""
+        switch (Number(zone)) {
+            case 50: result = "Zone 1"
+                break
+            case 60: result = "Zone 2"
+                break
+            case 70: result = "Zone 3"
+                break
+            case 80: result = "Zone 4"
+                break
+            case 90: result = "Zone 5"
+                break
+            default: result = "Unknown"
+        }
+        return result
+    }
+
     return (
-        <div className="intensity-results">
+        <div className="intensity-results flex gap-14">
             { selectedOptions.heartRate && heartRateList.length > 0 && (
-                <>
-                    <h1>Result of Pulse Zones</h1>
+                <div className="flex flex-col">
+                    <h1 className="mb-4">Result of Pulse</h1>
                     { heartRateList.map((item, index) => (
-                        <span key={`hr-zone-${index}`} >
-                            Zone {isAllZone ? index + 1 : selectedZone}: {item.toFixed(2)}
+                        <span className="flex mb-2" key={`hr-zone-${index}`} >
+                            {isAllZone ? `Zone: ${index + 1}` : mapSelectedZone(selectedZone)}: {item.toFixed(2)}
                         </span>
                     ))}
-                </>
+                </div>
             )}
             {selectedOptions.speed && speedList.length > 0 && (
-                <>
-                    <h1>Result of Speed Zones</h1>
+                <div className="flex flex-col">
+                    <h1 className="mb-4">Result of Speed</h1>
                     { speedList.map((item, index) => (
-                        <span key={`speed-zone-${index}`} >
-                            Zone {isAllZone ? index + 1 : selectedZone}: {item.toFixed(2)}
+                        <span className="flex mb-2" key={`speed-zone-${index}`} >
+                            {isAllZone ? `Zone: ${index + 1}` : mapSelectedZone(selectedZone)}: {item.toFixed(2)}
                         </span>
                     ))}
-                </>
+                </div>
             )}
             {selectedOptions.watt && wattList.length > 0 && (
-                <>
-                    <h1>Result of Watt Zones</h1>
+                <div className="flex flex-col">
+                    <h1 className="mb-4">Result of Watt</h1>
                     { wattList.map((item, index) => (
-                        <span key={`watt-zone-${index}`} >
-                            Zone {isAllZone ? index + 1 : selectedZone}: {item.toFixed(2)}
+                        <span className="flex mb-2" key={`watt-zone-${index}`} >
+                            {isAllZone ? `Zone: ${index + 1}` : mapSelectedZone(selectedZone)}: {item.toFixed(2)}
                         </span>
                     ))}
-                </>
+                </div>
             )}
         </div>
     )
