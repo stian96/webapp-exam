f03551e44ed8216563cd66134095c9136329f561
stian96 <stiankru@hiof.no>
2023-11-29 22:01:47 +0100

    fixed margin for reports page

--- oppgave_2/src/components/Report/ReportList.tsx
+++ oppgave_2/src/components/Report/ReportList.tsx
@@ -10,7 +10,7 @@ import { Icons } from "@/components/icons"
 export default function ReportLits() {
     const reportIds = ["1", "2", "3"]
     return (
-        <div>
+        <div className="mt-24">
             {reportIds.map((id) => (
                 <Link key={id} href={`/reports/${id}`} passHref>
                     <div className="card card__ReportList">
