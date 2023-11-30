ca937515ba0153ff0359f1147ccaf3ccd8466a30
LorenaHolmeide <89775879+LorenaHolmeide@users.noreply.github.com>
2023-11-30 19:43:26 +0100

    upadte seed.ts

--- oppgave_2/prisma/seed.ts
+++ oppgave_2/prisma/seed.ts
@@ -1,174 +1,145 @@
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
-/*
+
 async function main() {
+  // Create Performers
   const performer1 = await prisma.performers.create({
     data: {
-      userId: 'unique-idte2st1',
+      userId: 'Lars1',
       gender: 'Male',
-      sport: 'Swimming',
+      sport: 'swimming',
       heartRate: 85,
       watt: 190,
       speed: 25,
     },
   });
+
   const performer2 = await prisma.performers.create({
     data: {
-      userId: 'unique-idt3est2',
+      userId: 'Mona2',
       gender: 'Female',
-      sport: 'Cycling',
+      sport: 'cycling',
       heartRate: 65,
       watt: 220,
       speed: 35,
     },
   });
 
-  const sessionTemplate = await prisma.sessions.create({
-    data: {
-      name: 'cyclingTemplate',
-      type: 'cycling',
-      isTemplate: true,
-    },
-  });
+  const sessionTypes = ['cycling', 'swimming', 'running', 'triathlon'];
 
-  const sessionIdsForPerformer1 = [];
-  const sessionIdsForPerformer2 = [];
-  const sessionTypes = ['cycling', 'running', 'swimming', 'triathlon'];
+  // Define a function to create intervals and interval results
+  async function createIntervalsAndResults(sessionId: string) {
+    const interval = await prisma.intervals.create({
+      data: {
+        duration: 45,
+        intensity: 7,
+      },
+    });
+
+    await prisma.intervalResults.create({
+      data: {
+        intervalId: interval.id,
+        duration: interval.duration,
+        intensityMin: 5,
+        intensityMax: 9,
+        intensityAvg: 7,
+        pulseMin: 60,
+        pulseMax: 160,
+        pulseAvg: 110,
+        speedMin: 2,
+        speedMax: 5,
+        speedAvg: 3.5,
+        wattMin: 100,
+        wattMax: 400,
+        wattAvg: 250,
+      },
+    });
+
+    return interval.id;
+  }
 
   for (const type of sessionTypes) {
-    const session1 = await prisma.sessions.create({
-      data: {
-        name: `${type} Session`,
-        type: type,
-        isTemplate: false,
-        uniqueToPerformer: { connect: { id: performer1.id } },
-      },
-    });
-    sessionIdsForPerformer1.push(session1.id);
+    for (const performer of [performer1, performer2]) {
+      const session = await prisma.sessions.create({
+        data: {
+          name: `${type} Session`,
+          type: type,
+          isTemplate: false,
+          performerId: performer.id,
+        },
+      });
 
-    const session2 = await prisma.sessions.create({
-      data: {
-        name: `${type} Session`,
-        type: type,
-        isTemplate: false,
-        uniqueToPerformer: { connect: { id: performer2.id } },
-      },
-    });
-    sessionIdsForPerformer2.push(session2.id);
-  }
+      const sessionActivity = await prisma.sessionActivity.create({
+        data: {
+          date: new Date(),
+          sessionId: session.id,
+          performerId: performer.id,
+        },
+      });
 
+      await prisma.sessionTags.create({
+        data: {
+          sessionId: session.id,
+          tag: type.toLowerCase(),
+        },
+      });
 
-  const questionCreated = await prisma.questions.create({
-    data: {
-      question: 'How did you feel?',
-      type: 'text',
+      const question = await prisma.questions.create({
+        data: {
+          question: 'How demanding was the session',
+          type: 'text',
+        },
+      });
 
-    },
-  });
+      await prisma.sessionQuestions.create({
+        data: {
+          sessionId: session.id,
+          questionId: question.id,
+        },
+      });
 
-  const questionId = questionCreated.id;
-
-  const tag1 = 'hard';
-  const tag2 = 'cycling';
-
-
-  // Koble tags og questions til sessions for hver performer
-  for (const sessionId of sessionIdsForPerformer1) {
-    // Koble tag til session
-    await prisma.sessionTags.create({
-      data: {
-        session: { connect: { id: sessionId } },
-        tag: tag1,
-      },
-    });
-
-    // Koble question til session
-    await prisma.sessionQuestions.create({
-      data: {
-        session: { connect: { id: sessionId } },
-        question: { connect: { id: questionId } },
-      },
-    });
-  }
-
-  // Performer 2
-  for (const sessionId of sessionIdsForPerformer2) {
-    await prisma.sessionTags.create({
-      data: {
-        session: { connect: { id: sessionId } },
-        tag: tag2,
-      },
-    });
-
-    await prisma.sessionQuestions.create({
-      data: {
-        session: { connect: { id: sessionId } },
-        question: { connect: { id: questionId } },
-      },
-    });
-  }
+      const intervalId = await createIntervalsAndResults(session.id);
+      await prisma.sessionIntervals.create({
+        data: {
+          sessionId: session.id,
+          intervalId: intervalId,
+        },
+      });
 
 
+      // Create 3 Reports for each performer, skipping the last session
+      if (type !== 'triathlon') {
+        await prisma.reports.create({
+          data: {
+            status: 'Completed',
+            comments: `${type} session completed successfully`,
+            sessionActivityId: sessionActivity.id,
+          },
+        });
 
 
-  // Legger til SessionActivities for hver Performer
-  const sessionActivityIdsForPerformer1 = [];
-  const sessionActivityIdsForPerformer2 = [];
+      }
 
-  for (const sessionId of sessionIdsForPerformer1) {
-    const sessionActivity = await prisma.sessionActivity.create({
-      data: {
-        date: new Date(),
-        session: { connect: { id: sessionId } },
-        performer: { connect: { id: performer1.id } },
-      },
-    });
-    sessionActivityIdsForPerformer1.push(sessionActivity.id);
-  }
+      await prisma.reportIntervalResults.create({
+        data: {
+          reportId: Report.id,
+          intervalResultId: intervalResultId, // Replace this with the actual interval result ID
+        },
+      });
 
-  for (const sessionId of sessionIdsForPerformer2) {
-    const sessionActivity = await prisma.sessionActivity.create({
-      data: {
-        date: new Date(),
-        session: { connect: { id: sessionId } },
-        performer: { connect: { id: performer2.id } },
-      },
-    });
-    sessionActivityIdsForPerformer2.push(sessionActivity.id);
-  }
-
-
-  // Opprette Reports
-  for (let i = 0; i < 3; i++) {
-    await prisma.reports.create({
-      data: {
-        status: 'Completed',
-        comments: 'Great performance',
-        sessionActivity: { connect: { id: sessionActivityIdsForPerformer1[i] } },
-      },
-    });
-
-    await prisma.reports.create({
-      data: {
-        status: 'Completed',
-        comments: 'Excellent performance',
-        sessionActivity: { connect: { id: sessionActivityIdsForPerformer2[i] } },
-      },
-    });
+    }
   }
 
   console.log("Seed data created successfully");
 }
 
-
-
 main()
   .then(async () => {
-    await prisma.$disconnect()
+    await prisma.$disconnect();
   })
   .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })*/
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
